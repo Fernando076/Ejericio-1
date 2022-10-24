a4f56dfebf94ec6ac2985066a421ed18a1cb4ad7
Fernando076 <116199770+Fernando076@users.noreply.github.com>
2022-10-24 14:31:39 -0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_application_1/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Home());
+  }
+}
